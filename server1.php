89106e5a1c4320b008c8c44e59053ae8d9dd3301
yuanrui919 <55520350+yuanrui919@users.noreply.github.com>
2023-01-18 10:25:53 +0800

    Add files via upload

--- server1.php
+++ server1.php
@@ -32,7 +32,7 @@
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     # Get the response
     $response = curl_exec($ch);
     $error = curl_error($ch);
